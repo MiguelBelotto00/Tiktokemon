41f74c2216d2609a64ad915110c474eb27c9d256
MiguelBelotto00 <belottomiguel.00@gmail.com>
2022-06-29 16:50:29 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# tiktokemon
+
+Challenge desarrollado por petición de una empresa en el proceso de recruitmen.
+
+## Versiones
+- Flutter 3.0.0
+- Dart 2.17.0
+- DevTools 2.12.0
+
+## Librerias
+-[http 0.13.4](https://pub.dev/packages/http)
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tiktokemon/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTokemon',
+      initialRoute: 'home_page',
+      routes: {
+        'home_page': (BuildContext context) => const HomePage()
+      },
+    );
+  }
+}
